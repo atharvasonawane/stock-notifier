d19fcfe49c794be47aaf571c5c4dbd8b09994865
Shreyash Saitwal <40118018+shreyashsaitwal@users.noreply.github.com>
2023-11-28 20:26:36 +0530

    chore: update

--- stock_notifier/__main__.py
+++ stock_notifier/__main__.py
@@ -1,7 +1,13 @@
 import os
-import websocket
+import asyncio
+
 import json
+import websockets
 from dotenv import load_dotenv
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
 from stock_notifier.models import WebSocketResponse, WebSocketRequest
 
 load_dotenv()
@@ -14,33 +20,45 @@ def notify(symbol: str, curr: float, desired: float):
 
 
 def on_message(ws, message):
-    msg = WebSocketResponse(**json.loads(message))
-    trades_to_listen = filter(lambda item: item.symbol in to_listen.keys(), msg.data)
-    for trade in trades_to_listen:
-        prices = to_listen[trade.symbol]
-        to_notify = filter(lambda item: trade.last_price >= item, prices)
-        for price in to_notify:
-            notify(trade.symbol, price)
-    print(msg.data[0].last_price)
+    try:
+        msg = WebSocketResponse(**json.loads(message))
+        trades_to_listen = filter(
+            lambda item: item.symbol in to_listen.keys(), msg.data
+        )
+        for trade in trades_to_listen:
+            prices = to_listen[trade.symbol]
+            to_notify = filter(lambda item: trade.last_price >= item, prices)
+            for price in to_notify:
+                notify(trade.symbol, trade.last_price, price)
+    except Exception as e:
+        print(f"Message: {message}")
+        print(f"Exception: {e}")
 
 
-def on_open(ws):
-    # req = WebSocketRequest(type="subscribe", symbol="BINANCE:BTCUSDT")
-    ws.send('ping')
+async def websocket():
+    token = os.environ.get("FINNHUB_TOKEN")
+    async with websockets.connect(f"wss://ws.finnhub.io?token={token}") as ws:
+        symb = input("Enter symbol name: ")
+        threshold = float(input("Enter threshold price: "))
+        to_listen[symb] = [threshold]
+        req = WebSocketRequest(type="subscribe", symbol=symb)
+        await ws.send(req.model_dump_json())
+        while True:
+            on_message(ws, await ws.recv())
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+async def start_bot():
+    token = os.environ.get("TELEGRAM_API_TOKEN")
+    application = ApplicationBuilder().token(token).build()
+
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
 
 
 if __name__ == "__main__":
-    token = os.environ.get("FINNHUB_TOKEN")
-    ws = websocket.WebSocketApp(
-        f"wss://ws.finnhub.io?token={token}",
-        on_message=on_message,
-    )
-    # ws.
-    ws.on_open = on_open
-
-    symb = input("Enter symbol name: ")
-    price = input("Enter desired price: ")
-
-    req = WebSocketRequest(type="subscribe", symbol=symb)
-    ws.send(req)
-    ws.run_forever()
+    asyncio.run(websocket())
--- stock_notifier/models.py
+++ stock_notifier/models.py
@@ -7,12 +7,13 @@ class TradeData(BaseModel):
     symbol: str = Field(alias="s")
     timestamp: int = Field(alias="t")
     volume: float = Field(alias="v")
-    trade_conditions: List | None = Field(default=None, alias='c')
+    trade_conditions: List | None = Field(default=None, alias="c")
 
 
 class WebSocketResponse(BaseModel):
     data: List[TradeData]
     type: str
+
 
 class WebSocketRequest(BaseModel):
     type: str
