489d2240f4dc03cc0e2da20926c3d640aefbfa8d
Shreyash Saitwal <40118018+shreyashsaitwal@users.noreply.github.com>
2023-12-02 00:06:27 +0530

    feat: send notification on telegram on hitting trigger price

--- stock_notifier/__main__.py
+++ stock_notifier/__main__.py
@@ -1,64 +1,122 @@
 import os
 import asyncio
+from threading import Thread, Lock
+from typing import Dict, Tuple
 
 import json
-import websockets
+from websocket import WebSocketApp
 from dotenv import load_dotenv
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, Application
 
 from stock_notifier.models import WebSocketResponse, WebSocketRequest
 
-load_dotenv()
 
-to_listen = {}
-
-
-def notify(symbol: str, curr: float, desired: float):
-    print(f"ALERT: {symbol} has hit price: {curr} (your desired price: {desired})")
+TG_APP: Application
+SOCKET: WebSocketApp
+LOCK = Lock()
+SUBSCRIPTIONS: Dict[str, Tuple[str, float]] = {}
 
 
 def on_message(ws, message):
     try:
-        msg = WebSocketResponse(**json.loads(message))
-        trades_to_listen = filter(
-            lambda item: item.symbol in to_listen.keys(), msg.data
-        )
-        for trade in trades_to_listen:
-            prices = to_listen[trade.symbol]
-            to_notify = filter(lambda item: trade.last_price >= item, prices)
-            for price in to_notify:
-                notify(trade.symbol, trade.last_price, price)
+        with LOCK:
+            msg = WebSocketResponse(**json.loads(message))
+            trades_to_listen = filter(
+                lambda item: item.symbol in SUBSCRIPTIONS.keys(), msg.data
+            )
+            for trade in trades_to_listen:
+                subs = SUBSCRIPTIONS[trade.symbol]
+                for sub in subs:
+                    if trade.last_price >= sub[1]:
+                        asyncio.run(
+                            TG_APP.bot.send_message(
+                                chat_id=sub[0],
+                                text=f"""
+📢 Price Alert!
+**{trade.symbol}** is now at **{trade.last_price}**! ()
+""",
+                            )
+                        )
+                    SUBSCRIPTIONS[trade.symbol].remove(sub)
+
     except Exception as e:
         print(f"Message: {message}")
         print(f"Exception: {e}")
 
 
-async def websocket():
+def start_websocket():
     token = os.environ.get("FINNHUB_TOKEN")
-    async with websockets.connect(f"wss://ws.finnhub.io?token={token}") as ws:
-        symb = input("Enter symbol name: ")
-        threshold = float(input("Enter threshold price: "))
-        to_listen[symb] = [threshold]
-        req = WebSocketRequest(type="subscribe", symbol=symb)
-        await ws.send(req.model_dump_json())
-        while True:
-            on_message(ws, await ws.recv())
+    global SOCKET
+    SOCKET = WebSocketApp(f"wss://ws.finnhub.io?token={token}", on_message=on_message)
+    SOCKET.run_forever()
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="""
+📈 Welcome to Stock Notifier Bot! 🔔
+
+To subscribe to a stock, use the following format:
+`/subscribe {stock-symbol} {trigger-price}`
+
+For example:
+`/subscribe AAPL 120`
+""",
+    )
 
 
-async def start_bot():
-    token = os.environ.get("TELEGRAM_API_TOKEN")
-    application = ApplicationBuilder().token(token).build()
+async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    split = update.message.text.split(" ")
+    if len(split) != 3:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="""
+‼️ Error: invalid format. 
+
+Please use the following format:
+`/subscribe {stock-symbol} {trigger-price}`
+
+For example:
+`/subscribe AAPL 120`
+""",
+        )
+        return
+
+    symbol = split[1]
+    trigger = float(split[2])
+    if symbol not in SUBSCRIPTIONS.keys():
+        SUBSCRIPTIONS[symbol] = [(update.effective_chat.id, trigger)]
+        req = WebSocketRequest(type="subscribe", symbol=symbol)
+        SOCKET.send(req.model_dump_json())
+    else:
+        SUBSCRIPTIONS[symbol].append((update.effective_chat.id, trigger))
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"""
+✅ Subscription successful!
+You will be notified when **{symbol}** hits **{trigger}** or heigher.
+""",
+    )
+
+
+def start_bot():
+    global TG_APP
+    TG_APP = ApplicationBuilder().token(os.environ.get("TELEGRAM_API_TOKEN")).build()
 
     start_handler = CommandHandler("start", start)
-    application.add_handler(start_handler)
+    subscribe_handler = CommandHandler("subscribe", subscribe)
 
-    application.run_polling()
+    TG_APP.add_handler(start_handler)
+    TG_APP.add_handler(subscribe_handler)
+    TG_APP.run_polling()
 
 
 if __name__ == "__main__":
-    asyncio.run(websocket())
+    load_dotenv()
+    ws_thread = Thread(target=start_websocket)
+    ws_thread.start()
+    start_bot()
+    ws_thread.join()
